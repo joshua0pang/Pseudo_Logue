e05f3dbc98d0375271faccc3212028ee13195a65
joshua0pang <Yashasvi@Joshuas-MacBook-Pro.local>
2018-02-13 11:04:42 -0800

    The sleeper
    has awakened

---
+++ Z_Code_Psuedo.go
@@ -0,0 +1,237 @@
+The Geometer and The Grammarian
+by Josh Pang
+
+Preface // Read Me
+
+"Welcome to the Geometer and the Grammarian. 
+In this novella, we'll be doing a sort of call and response to Pseudo Code, and adding a drop of Synergetics — you won't even notice. 
+Good Luck, Have fun!"
+
+// Chapter 1: The Tetrahedrons of Universe
+// An Introduction to Phenomenology
+
+The Tetrahedons of Universe eternally regenerate.
+Every day the Tetrahedrons expand to Octahedrons, then expand to Icosahedrons, then contract to Octahedrons, then contract to Tetrahedrons.
+Their various costumes are expressible by the vector equilibrium.
+The Tetrahedrons have 12 degrees of freedom, 6 positive degrees and 6 negative degrees. 
+
+Moral Of The Story: Tetrahedrons are the basic structure in universe. 
+In science we call this Synergetics.
+
+// Chapter 2: Geometer Josh
+// Why we need synergy
+
+Josh thinks he is 100% enlightened. 
+He accepts his stream of consciousness as the moving image of eternity, "Hello little tetrahedron, you can be whatever you like."
+He attributes his unusual vanity to non-attachment — "Who am I to be attached to constantly intertransforming tetrahedrons? 
+"What's there to be attached to?" 
+He smiles as he drinks his delightful beverage: first-things-first. 
+Josh is quite a good geometer. 
+He has an insane amount of tetrahedrons. 
+In fact, Josh is getting a little overwhelmed by his success. 
+He needs to stop missing so many tiny 'trivial' details, as they come back to haunt him. 
+"Oh little tetrahedrons, what will I do?"
+That night, Joshua finds the solution he needs in a dream.
+
+Moral Of The Story: Geometry Josh needs "syntactical incisivity" to manage the plethora of tetrahedrons in his mind. 
+In Synergetics, we need syntactical incisivity to communicate to a popular audience.
+
+// Chapter 3: Geometry Josh Dreams A Solution
+// An Introduction to Precognitive Dreaming
+
+Joshua is always the last to awaken in his neighborhood.
+Every morning he initiates his consciousness begrudgingly. 
+Very begrudgingly.
+Essentially, Geometry Josh is lazy.
+Seemingly lazy, anyway.
+The first thing he does every morning is drink coffee. 
+After a good cup, he checks the internet for news of peace.
+Joshua greets his friend Mr. Computer the same way every morning, "Hello old bean!"
+Joshua has greeted each day every day in this way all his life. 
+This is his entrance into World Game — "Hello all humanity, I've travelled a long way from the world of dreams. 
+I know just what to do today, just give me a little time."
+Slowly but surely, Joshua drags himself out of bed and does his hygiene habits. 
+He also eats something, because without food he cannot think or speak or act.
+
+In his dreams, Joshua sees the future. 
+How does this happen? 
+Maybe the nature of time is different than we all suppose. 
+Maybe we all are connected at some supraconscious wavelength, and so he just receives word about what we had all previously agreed on.
+No one knows, right?
+
+There are two things Geometry Josh knows. 
+Sanskrit will become the planetary language. 
+And Synergetics will revolutionize all modern science. 
+
+Joshua is very happy when he dreams the future, and he is often impressed about how much his life is able to be normal 
+despite such abnormal circumstances. 
+Geometry Josh has an intuitive feel in his very body about how things fit together geometrically. 
+As a person, he simply does not need to 'know' so many things. 
+A lot of his life is just going along with the inexorable tide of evolution.
+
+Joshua has made friends with certain large patterns. 
+They are the Kapha Tetrahedron, the Pitta Octahedron, and the Vata Icosahedron. 
+It turns out, Ayurveda is really something special. 
+Josh simply files Ayurveda under Synergetic knowledge expressed in Sanskrit.
+
+Kapha is a tetrahedron because it is solid and simple. 
+Pitta is an octahedron because it is more complex and sharper. 
+Vata is an icosahedron because it is simply the most complicated. 
+Of all the shapes in our Universe, Vata shapes are the dominant source of all problems until the end of time. 
+
+There are a lot of patterns worth learning about between Sanskrit and Synergetics. 
+For example, comparing ॐ to the Vector Equilibrium. 
+And precisely mapping the macro-wisdom of Yoga with Synergetics. 
+These are some of the projects Geometry Josh will undertake when the world knows peace. 
+Until then, there is a real emergency our friend Josh devotes himself to addressing in the mean time. 
+See his World Game thesis.package main
+
+Moral of the Story: Knowing the farthest futures is fun and fulfilling, call it thrilling; however, until the present moment 
+is handled properly, we are anything but with integrity in applying ourselves otherwise. 
+
+// Chapter 4: Joshua Finds Joy In Young Ms. Grammarian's Word Choice
+// Unity is plural and at minimum is two
+
+Joshua, being a hardcore geometer, is used to interacting with the strange. 
+A lot of his experience is actually pretty psychedlic — "WHOA! Did you SEE that?!"
+
+Young Ms. Grammarian usually has a social following. 
+Since Ms. Grammar is the center of so much social attention, she has had to grow into a role of micro-perfection. 
+In turn, as everyone notices everything she does, she notices everything anyone does — all within her field of awareness. 
+In a flash, Ms. Grammarian knows your whole life when you meet her for the first time. 
+And she drops a subtle word or two to point you in the right direction.
+
+Once upon a time, Young Ms. Grammarian and Geometer Josh met each other in the temple on the holy mountain of ambrosia. 
+Il Caffé. 
+Coffee is an excellent way to communicate micro-incisively, as it heightens the connection to waking reality. 
+
+Their relationship began as a kind of extremely formal first contact between disparate cultures. 
+Over time, Geometry Josh began to notice his internal geometry changing — and quite a lot ! 
+Energy started to behave in very new ways completely beyond his control. 
+This, being a usually competent geometer, at the helm of his own tetrahedroning, gave him pause. 
+He took notice. 
+And, using the reasoning skills of geometry — deducing an unknown from the known — he figured out what — or who — was 
+fiddling with his carefully arranged vector equilibrium!
+
+It turns out, in some mysterious synergy, it was Young Ms. Grammarian and her mere proximity which was turvying all his topsies. 
+"Was it an unusual configuration of movement through the 12 degrees of freedom? 
+How could this be happening? 
+And to me of all people?"
+
+Still assuming he was totally enlightened, Geometry Josh chose to go with the flow, though at times containing his curiosity was difficult.
+
+Little did Josh know this was going to became a major vortex for the foreseeable future.
+
+Moral of the Story: There are two sides to every coin, and — by universal law — no matter how competent and omnipotent and omniscient 
+a person may or may not be — there is always always always a blindspot, a 'bizarro realm' in which someone else is inside-out you. 
+Everyone sees in 180 degrees. 
+No one sees in 360 degrees. 
+It takes two to tango, and to get the big picture. 
+
+// Chapter 5: Joshua Chats With Young Ms. Grammarian
+// Synergetics explains how English can sound an awful lot like Sanskrit — mostly subconsciously.
+
+Every Saturday for as long as he could remember, Joshua went to visit his best friend the Grammarian. 
+On one unusual Saturday, Ms. Grammarian accused Geometry Josh of not knowing everything about Sanskrit — "I thought you knew everything" 
+she said, with a tone only she could produce. 
+Determined to find out for himself if he did or did not know everything, Geometry Josh embarked on a quest.
+Searching through all possible aspects of her challenge was time consuming, yet it was the most important thing to be thinking about.
+After studying Italian Grammar, immersing himself in the culture and food and fashion; in the lifestyle and the slowness and the emotion:
+even the gesticulations of body language; after all this, a simple conclusion was found.
+Though Sanskrit is indeed the most efficient form of communication, the language of enlightenment and the riches of bliss, joy, and ecstasy 
+-- even though all of this is true, there is a kind of niché each language and culture has come to be proficient in.
+For English, the niché is commerce. 
+For Italian, the niché is music. 
+All natural languages are a kind of heuristic for particular types of thinking and feeling and doing. 
+This was a liberating realization — mathematics is a kind of language with a niché for precise expression. 
+It's all just a bunch of language.
+Yet, if this were true, why haven't we all eventually stumbled into Sanskrit over so many millenia?
+Sanskrit, which is the natural language for humanly-maximum thinking, feeling, and doing.
+The language which embraces all nichés. 
+The dictionary with the words we've all look for at each stage of life.
+
+Synergetics has an explanation. 
+There is a layer beneath the semantics of the spoken word. 
+That layer is one of sound harmonics — one well beyond our normal functioning of an ego. 
+While our semantics can often be illusory, the underlying sound harmonics is always real. 
+There really isn't anything "unreal" — by definition. 
+There can be inaccuracies, and pointing to one thing yet meaning another, etc. 
+These can sometimes be useful, and a lot of times not. 
+The sound we make is always real, and is in fact the stuff of reality. 
+They are the tetrahedrons behind our spoken words. 
+Though we have an inner monologue, in whatever random language we use, there are tetrahedrons which are running the show, sometimes 
+unbeknownst even to us. 
+And sound has a direct connection to the elemental tetrahedrons. 
+The sounds we make may actually be 'programming' our local environment.
+Wouldn't it be nice?
+Our words, however, are one step removed.
+That is where the very real danger of miscommunication occurs.
+Young Ms. Grammarian's challenge brought this to light for our friend Geometry Josh. 
+He hopes they will spend a lot of time drinking coffee and being friendly with one another.
+The Geometer has a good grasp on the context of reality. 
+While the Grammarian has a good grasp on the content of reality. 
+Without the Grammarian, the Geometer is effectively-mute — much like Nature herself.
+Is Nature un-ergonomic?
+Is she having trouble communicating with us?
+Without the Geometer, the Grammarian is impotent — full of sound and fury signifying nothing. 
+Are humans un-Natural?
+Are we having trouble communicating with her?
+
+Between our two friends, the Geometer and the Grammarian, we may one day come to see Sanskrit as everyone's mother tongue, and Synergetics 
+as everyone's operating system.
+In some ways, geometry is about hardware, and grammar is about software — we need to evolve both concurrently.
+And indeed, Sanskrit is the best language for generating the software of culture — the words which color our experience constantly in the 
+waking world. 
+And indeed, Synergetics is the best way for generating the hardware of culture — geodesic domes for example, also the content of dreams and
+thoughts you know.
+
+Geometry Josh is verry happy to have made the acquaintance of a friendly Grammarian.
+
+// Chapter 6: Young Ms. Grammarian Finds A Way To Be Near Her Dear Friend Josh
+// Tiptoe, Through the window, By the window, That is where I'll be, Come tiptoe, Through the tulips, With me
+
+Father Time and Mother Nature have a secret to their happy marriage. Humans! Humans create the necessary spice of life to keep their cosmic 
+food interesting neverendingly. Geometry Josh has a favorite line from A Midsummer Night Dream. "Ill met by moonlight, proud Titania." 
+The ancient words of the fairy king Oberon show engagement in some very advanced Yin and Yang stuff. For all their eternality, 
+Shakespeare's royal faeries are all too human.
+Direct communication is the key to the tensional integrity of a relationship. Young Ms. Grammarian, being the very powerful linguist that 
+she is, had gotten all wrapped up in the subtleties and nuances of feigns and smiles that she had gotten a little lost in her own narrative. 
+And Geometry Josh, for all his order and consistency, was the worse at normal direct communication for the overwhelming synchronicity that 
+accompanies manipulating the unified field of cosmically hierarchical omni-interrationally-phased, Nuclear-centered, Convergently-
+divergently Intertransformable Systems of Geometry: a succession of concentric, 12-around-one, closest-packed spheres, each of a 
+tetravolume of five, i.e. of 120 A and B Quanta Modules omniembracing the hierarchy of nuclear event patternings — the synergetics 
+isometric view of the isotropic vector matrix and its omnirational, low-order whole number, equilibrious state of the micro-macro cosmic 
+limits of the nuclearly unique, symmetrical morphological relativity and its interquantatative, intertranformative, intertransactive 
+expansive-contractive, axially rotative, operational field which, as an operationally transformable complex, is conceptualizable only in 
+its equilibrious state (-rbf) — is naturally going to stun people. These are a lot of radiant photons for literally anybody.
+After years of back and forth, highs and lows — "omnidirectional" — Young Ms. Grammarian lighted upon an insight. While apart, Geometry 
+Josh was extrodinarily unusual and easy to misunderstand; when together, Geometry Josh was simple and friendly and fun to be with: almost 
+addictive to be around for those who enjoy the certain Sanskrit ethers. The solution was ever so clear. 
+In this fictional fairy tale, anyway.
+She would simply have to marry him. 
+Everyone gets married in children's stories, isn't it, like, a rule?
+I don't want to break the rules!
+
+Binded by a ring, they would be complementary opposites. The perfect fit.
+Instantaneously she knew what she had to do. Utterly vulnerable, she expressed her experience in a way even Geometry Josh would comprehend. 
+She proposed, tying a colored string around his finger. What would Josh do? Of course he accepted Young Ms Grammarians's hand in marriage. 
+There's this girl he likes. She's the girl. And, they lived happily ever after do you know?
+
+Author's Endnote
+|| You talking money, need a hearing aid /   ||
+|| You talking 'bout me, I don't see a shade || 
+  -Starboy, The Weekend
+
+So yea, I do generally think women are superior beings in a finite numerical sense. However, in an infinitely regenerative universe sense, 
+where equilibrium is achieved only transcendentally, men and women are perfectly equal in their complementarity. While Sanskrit has three 
+genders, I am convinced by duality. If I am singularly wrong, that's okay, I live my life already assuming I'm always wrong and she's 
+always right. It's simpler that way. Even when she's wrong, she's right. Oh well. 
+Being a man is a thing, and I think we guys do have some special bond only we know, and vice versa, and those stuck in the middle. On 
+behalf of all men, I formally apologize to womankind for past, present, and future mistakes. Mistakes were made, mistakes are being made, 
+and more mistakes will be made. Guys, let's get our shit together. Yeah, it's like that. Can't live with us, can't live without us...
+All this to say, there is a synergy that arises which is by definition not predicted by the experiences of men and women apart. There's 
+some kind of joy that can only be sparked when opposites attract. Love one another, within this moment there is a total communcation being 
+simultaneously transmitted between opposite beings which will take all of time to complete the two-way transfer of Nature's polar energies. 
+Given this fact, have a little mercy on each other. 
+
+-Josh "The Geometer" Pang  
